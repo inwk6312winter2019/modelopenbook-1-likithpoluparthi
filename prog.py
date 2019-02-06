f0f003055e2bc1f4b70e917e7219d4c3b681278f
AKP96 <ajith.kumarperumal@dal.ca>
2019-02-06 09:56:51 -0800

    final

--- prog.py
+++ prog.py
@@ -1,9 +1,15 @@
-file =open("running-config.cfg",'r')
-for line in file:
-  line=line.strip()
-  line=line.split("!")
-  
-"""d=dict()
-def list_ifname_ip():
-  for interface in content:"""
-    
+file=open("running-config.cfg",'r')
+d=dict()
+def list_ifname_ip(file):  
+    for line in file:
+        if '!' not in line:
+            if 'nameif' in line:
+                line=line.strip()
+                key=line.split(" ")
+            if 'ip address' in line and '.' in line:
+                line=line.strip()
+                iptemp=line.split(" ")
+                tup=(iptemp[2],)
+                d.setdefault(key[1],(iptemp[2],iptemp[3]))
+    return d
+print(list_ifname_ip(file))
